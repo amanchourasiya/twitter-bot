db5f01aadd8f8186e45ef16881449ccf9a3b0da4
aman chourasiya <amanchourasia6@gmail.com>
2020-07-31 09:49:45 +0530

    Added data formatter

--- botutils/utils.py
+++ botutils/utils.py
@@ -4,8 +4,10 @@ import sys
 def get_logger():
     logger = logging.getLogger(name='twitter-bot')
     logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
         handler.setLevel(logging.INFO)
+        handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
